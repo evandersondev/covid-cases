443736b06fbf5567a1057b719f470dd12f250951
evandersondev <evandersonvasconcelos23@gmail.com>
2020-06-10 22:45:44 -0300

    updating headrs on api

--- src/services/api.js
+++ src/services/api.js
@@ -4,22 +4,27 @@ const api = create({
   baseURL: 'http://coronavirus-19-api.herokuapp.com/countries',
 })
 
-const headers = {
-  mode: 'cors',
-  cache: 'default',
-}
-
 export default {
   async getCasesByCountry(country = 'World') {
     const {
       data: { todayCases, todayDeaths, deaths, recovered },
-    } = await api.get(`/${country}`, headers)
+    } = await api.get(`/${country}`, {
+      headers: {
+        mode: 'cors',
+        cache: 'default',
+      },
+    })
 
     return { todayCases, todayDeaths, deaths, recovered }
   },
 
   async getNamesAllCountries() {
-    const { data } = await api.get('/', headers)
+    const { data } = await api.get('/', {
+      headers: {
+        mode: 'cors',
+        cache: 'default',
+      },
+    })
     const countries = data.map(({ country }) => country)
 
     return countries
